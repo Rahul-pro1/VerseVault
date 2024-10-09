6aef49cd1367f06e0fc8af13e74c19c458f7e07d
rahul-pro1 <rahulsiv2108@gmail.com>
2024-10-09 15:01:24 +0530

    First commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv'
+import { getCustomers } from './index.js';
+
+dotenv.config()
+
+console.log("APP ", process.env.CORS_ORIGIN);
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({
+    limit: "16kb" 
+}))
+
+app.use(express.urlencoded({
+    extended: true, limit: "16kb"
+}))
+
+app.use(express.static("public")) //For any other files like images etc, same name folder must be created in root dir
+
+app.use(cookieParser()) 
+
+app.get('/', async (req, res) => {
+    const records = await getCustomers();
+    return res.json(records);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}...`)
+})
